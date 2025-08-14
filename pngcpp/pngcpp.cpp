083032cdbcdeede8b92172e4365c19f4b58dbe98
0xAA55 <weixiaobao213@qq.com>
2025-08-14 11:03:45 +0800

    添加 libpng 的 C++ 封装。

---
+++ pngcpp/pngcpp.cpp
@@ -0,0 +1,176 @@
+#include "pngcpp.hpp"
+
+#include <png.h>
+#include <stdio.h>
+
+namespace pngcpp
+{
+	InvalidArgument::InvalidArgument(const std::string &what) noexcept:
+		std::exception(what.c_str())
+	{
+	}
+	LoadPngException::LoadPngException(const std::string &what) noexcept :
+		std::exception(what.c_str())
+	{
+	}
+	SavePngException::SavePngException(const std::string &what) noexcept :
+		std::exception(what.c_str())
+	{
+	}
+	class File
+	{
+	protected:
+		FILE *fp;
+
+	public:
+		File() = delete;
+		File(File &&) = default;
+		File(const File &) = delete;
+		File(const char *path, const char *mode) : fp(fopen(path, mode))
+		{
+			if (!fp) throw SavePngException(std::string("Could not open the file `") + path + "` for write.");
+		}
+		~File()
+		{
+			fclose(fp);
+		}
+
+		FILE *get() const
+		{
+			return fp;
+		}
+	};
+	class png_writer
+	{
+	protected:
+		png_structp p;
+		png_infop i;
+
+	public:
+		png_writer() : p(png_create_write_struct(PNG_LIBPNG_VER_STRING, const_cast<void *>(reinterpret_cast<const void *>(this)), reinterpret_cast<png_error_ptr>(on_png_error), NULL))
+		{
+			i = png_create_info_struct(p);
+			if (!i)
+			{
+				png_destroy_write_struct(&p, nullptr);
+				throw SavePngException(std::string("The function `png_create_info_struct()` had failed."));
+			}
+		}
+		~png_writer()
+		{
+			png_destroy_write_struct(&p, &i);
+		}
+
+		void write_png_rgba(const std::string &path, uint32_t width, uint32_t height, const Pixel * const*row_pointers) const
+		{
+			auto fp = File(path.c_str(), "wb");
+
+			png_init_io(p, fp.get());
+			png_write_png(p, i, 0, nullptr);
+
+			png_set_IHDR(p, i, width, height, 32,
+				PNG_COLOR_TYPE_RGBA,
+				PNG_INTERLACE_NONE,
+				PNG_COMPRESSION_TYPE_DEFAULT,
+				PNG_FILTER_TYPE_DEFAULT);
+
+			png_color_8 sig_bit = {8, 8, 8, 0, 8,};
+			png_set_sBIT(p, i, &sig_bit);
+			png_write_info(p, i);
+			png_write_image(p, const_cast<uint8_t**>(reinterpret_cast<const uint8_t* const*>(row_pointers)));
+			png_write_end(p, i);
+		}
+
+		void write_png_8bit(const std::string &path, uint32_t width, uint32_t height, const std::vector<Rgb> palette, const uint8_t *const*row_pointers) const
+		{
+			auto fp = File(path.c_str(), "wb");
+
+			png_init_io(p, fp.get());
+			png_write_png(p, i, 0, nullptr);
+
+			png_set_IHDR(p, i, width, height, 8,
+				PNG_COLOR_TYPE_PALETTE,
+				PNG_INTERLACE_NONE,
+				PNG_COMPRESSION_TYPE_DEFAULT,
+				PNG_FILTER_TYPE_DEFAULT);
+
+			png_set_PLTE(p, i, reinterpret_cast<png_const_colorp>(&palette[0]), palette.size());
+			png_write_info(p, i);
+			png_write_image(p, const_cast<uint8_t **>(reinterpret_cast<const uint8_t *const*>(row_pointers)));
+			png_write_end(p, i);
+		}
+
+	private:
+		static void on_png_error(void *self, const char *message)
+		{
+			throw SavePngException(std::string(message));
+		}
+	};
+
+	PngImage::PngImage(uint32_t width, uint32_t height, const Pixel &default_color) :
+		width(width), height(height)
+	{
+		pixels.resize(static_cast<size_t>(width) * height);
+		for (size_t i = 0; i < pixels.size(); i++) pixels[i] = default_color;
+		for (size_t y = 0; y < height; y++) row_pointers.push_back(&pixels[y * width]);
+	}
+	PngImage::PngImage(const std::string &path)
+	{
+		png_image image = { 0 };
+		image.version = PNG_IMAGE_VERSION;
+		if (png_image_begin_read_from_file(&image, path.c_str()))
+		{
+			image.format = PNG_FORMAT_RGBA;
+			width = image.width;
+			height = image.height;
+			pixels.resize(static_cast<size_t>(width) * height);
+			for (size_t y = 0; y < height; y++) row_pointers.push_back(&pixels[y * width]);
+			if (!png_image_finish_read(&image, NULL, &pixels[0], 0, NULL))
+			{
+				throw LoadPngException(std::string("Read png file failed: \"") + path + "\"");
+			}
+		}
+		else
+		{
+			throw LoadPngException(std::string("Could not load png file: \"") + path + "\"");
+		}
+	}
+	uint32_t PngImage::get_width() const
+	{
+		return width;
+	}
+	uint32_t PngImage::get_height() const
+	{
+		return height;
+	}
+	const std::vector<Pixel *> &PngImage::get_row_pointers() const
+	{
+		return row_pointers;
+	}
+	const Pixel &PngImage::get_pixel(uint32_t x, uint32_t y) const
+	{
+		if (x >= width || y >= height) throw InvalidArgument(std::string("Invalid coordinate (") + std::to_string(x) + ", " + std::to_string(y) + ")");
+		return row_pointers[y][x];
+	}
+	Pixel &PngImage::get_pixel(uint32_t x, uint32_t y)
+	{
+		if (x >= width || y >= height) throw InvalidArgument(std::string("Invalid coordinate (") + std::to_string(x) + ", " + std::to_string(y) + ")");
+		return row_pointers[y][x];
+	}
+	void PngImage::set_pixel(uint32_t x, uint32_t y, const Pixel &pixel)
+	{
+		if (x >= width || y >= height) throw InvalidArgument(std::string("Invalid coordinate (") + std::to_string(x) + ", " + std::to_string(y) + ")");
+		row_pointers[y][x] = pixel;
+	}
+	void PngImage::save_png32_to(const std::string &path) const
+	{
+		auto w = png_writer();
+		w.write_png_rgba(path, width, height, &row_pointers[0]);
+	}
+	void PngImage::save_png8_to(const std::string &path, uint32_t width, uint32_t height, const std::vector<Rgb> palette, const uint8_t *const *row_pointers)
+	{
+		auto w = png_writer();
+		w.write_png_8bit(path, width, height, palette, &row_pointers[0]);
+	}
+
+}
---
+++ pngcpp/pngcpp.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <cinttypes>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+namespace pngcpp
+{
+#pragma pack(push, 1)
+	struct Rgb
+	{
+		uint8_t R;
+		uint8_t G;
+		uint8_t B;
+
+		Rgb() = default;
+		Rgb(uint8_t R, uint8_t G, uint8_t B) : R(R), G(G), B(B) {}
+	};
+
+	struct Rgba: public Rgb
+	{
+		uint8_t A;
+
+		Rgba() = default;
+		Rgba(uint8_t R, uint8_t G, uint8_t B, uint8_t A) : Rgb(R, G, B), A(A) {}
+	};
+
+	union Pixel
+	{
+		Rgba rgba;
+		uint32_t u32;
+	};
+#pragma pack(pop)
+
+	class InvalidArgument : public std::exception {
+	public:
+		InvalidArgument(const std::string &what) noexcept;
+	};
+
+	class LoadPngException : public std::exception {
+	public:
+		LoadPngException(const std::string &what) noexcept;
+	};
+
+	class SavePngException : public std::exception {
+	public:
+		SavePngException(const std::string &what) noexcept;
+	};
+
+	class PngImage
+	{
+	protected:
+		std::vector<Pixel> pixels;
+		std::vector<Pixel *> row_pointers;
+		uint32_t width;
+		uint32_t height;
+
+	public:
+		PngImage() = delete;
+		PngImage(PngImage &&) = default;
+		PngImage(const PngImage &) = default;
+		PngImage(uint32_t width, uint32_t height, const Pixel& default_color);
+		PngImage(const std::string& path);
+
+		uint32_t get_width() const;
+		uint32_t get_height() const;
+
+		const std::vector<Pixel *>& get_row_pointers() const;
+		const Pixel &get_pixel(uint32_t x, uint32_t y) const;
+		Pixel &get_pixel(uint32_t x, uint32_t y);
+		void set_pixel(uint32_t x, uint32_t y, const Pixel& pixel);
+
+		void save_png32_to(const std::string &path) const;
+		static void save_png8_to(const std::string &path, uint32_t width, uint32_t height, const std::vector<Rgb> palette, const uint8_t *const *row_pointers);
+	};
+};
