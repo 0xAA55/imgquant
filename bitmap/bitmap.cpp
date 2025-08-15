2940c91c8b62913388c91fa4ebd3319b6a99c695
0xAA55 <weixiaobao213@qq.com>
2025-08-15 18:38:08 +0800

    因为 `Bitmap` 的模板实例化已经固定，所以直接将大多数的实现搬至 `bitmap.cpp` 文件。自己实现移动构造和赋值运算符，以实现正确管理内存。

--- bitmap/bitmap.cpp
+++ bitmap/bitmap.cpp
@@ -1,7 +1,127 @@
 #include "bitmap.hpp"
 
+#include <stdexcept>
+
+#include <cstdlib>
+#include <cassert>
+
 namespace bitmap
 {
+	template<typename T>
+	Bitmap<T>::Bitmap(const Bitmap& copy) :
+		Bitmap(copy.width, copy.height, copy.get_row_pointers())
+	{
+	}
+
+	template<typename T>
+	Bitmap<T>& Bitmap<T>::operator =(const Bitmap& to)
+	{
+		free(bitmap);
+		row_pointers.clear();
+		width = to.width;
+		height = to.height;
+		pitch = to.pitch;
+		build_bitmap();
+		copy_bitmap(to.get_row_pointers());
+		return *this;
+	}
+
+	template<typename T>
+	Bitmap<T>::Bitmap(Bitmap&& move) noexcept :
+		bitmap(move.bitmap),
+		row_pointers(std::move(move.row_pointers)),
+		pitch(move.pitch),
+		width(move.width),
+		height(move.height)
+	{
+		move.bitmap = nullptr;
+	}
+
+	template<typename T>
+	Bitmap<T>::Bitmap(uint32_t width, uint32_t height) :
+		width(width), height(height), pitch(calc_pitch(width))
+	{
+		build_bitmap();
+	}
+
+	template<typename T>
+	Bitmap<T>::Bitmap(uint32_t width, uint32_t height, const T& default_color) :
+		width(width), height(height), pitch(calc_pitch(width))
+	{
+		build_bitmap();
+#pragma omp parallel for
+		for (std::ptrdiff_t y = 0; y < static_cast<std::ptrdiff_t>(height); y++)
+		{
+			auto dst_row = row_pointers[y];
+			for (size_t x = 0; x < width; x++)
+			{
+				dst_row[x] = default_color;
+			}
+		}
+	}
+
+	template<typename T>
+	Bitmap<T>::Bitmap(uint32_t width, uint32_t height, const T*const* from_row_pointers) :
+		width(width), height(height), pitch(calc_pitch(width))
+	{
+		build_bitmap();
+		copy_bitmap(from_row_pointers);
+	}
+
+	template<typename T>
+	void Bitmap<T>::build_bitmap()
+	{
+		uint8_t *ptr = reinterpret_cast<uint8_t*>(malloc(pitch * height));
+		if (!ptr) throw std::bad_alloc();
+		bitmap = reinterpret_cast<T*>(ptr);
+		row_pointers.resize(height);
+		for (size_t y = 0; y < height; y++)
+		{
+			row_pointers[y] = reinterpret_cast<T*>(&ptr[pitch * y]);
+		}
+	}
+
+	template<typename T>
+	void Bitmap<T>::copy_bitmap(const T*const* from_row_pointers)
+	{
+#pragma omp parallel for
+		for (std::ptrdiff_t y = 0; y < static_cast<std::ptrdiff_t>(height); y++)
+		{
+			auto src_row = from_row_pointers[y];
+			auto dst_row = row_pointers[y];
+			for (size_t x = 0; x < width; x++)
+			{
+				dst_row[x] = src_row[x];
+			}
+		}
+	}
+
+	template<typename T>
+	template<typename T_dst>
+	Bitmap<T_dst> Bitmap<T>::convert(T_dst(*convert_fn)(T& src)) const
+	{
+		auto ret = Bitmap<T_dst>(width, height);
+		auto ret_row = ret.get_row_pointers();
+#pragma omp parallel for
+		for (std::ptrdiff_t y = 0; y < static_cast<std::ptrdiff_t>(height); y++)
+		{
+			auto src_row = row_pointers[y];
+			auto dst_row = ret_row[y];
+			for (size_t x = 0; x < width; x++)
+			{
+				dst_row[x] = convert_fn(src_row[x]);
+			}
+		}
+
+		return ret;
+	}
+
+	template<typename T>
+	Bitmap<T>::~Bitmap()
+	{
+		free(bitmap);
+	}
+
 	template class Bitmap<int>;
 	template class Bitmap<uint8_t>;
 	template class Bitmap<ColorRgb>;
--- bitmap/bitmap.hpp
+++ bitmap/bitmap.hpp
@@ -15,109 +15,87 @@ namespace bitmap
 	class Bitmap
 	{
 	protected:
-		std::vector<T> bitmap;
+		T* bitmap = nullptr;
 		std::vector<T*> row_pointers;
-		size_t pitch;
-		uint32_t width;
-		uint32_t height;
+		size_t pitch = 0;
+		uint32_t width = 0;
+		uint32_t height = 0;
 
 		static size_t calc_pitch(uint32_t width)
 		{
 			return ((static_cast<size_t>(width) * sizeof(T) - 1) / 4 + 1) * 4;
 		}
-		void build_bitmap()
-		{
-			bitmap.resize(pitch * height);
-			row_pointers.resize(height);
-			for (size_t y = 0; y < height; y++)
-			{
-				row_pointers[y] = reinterpret_cast<T*>(reinterpret_cast<uint8_t*>(&bitmap[0]) + pitch);
-			}
-		}
+		void build_bitmap();
+		void copy_bitmap(const T*const* from_row_pointers);
 
 	public:
 		Bitmap() = default;
-		Bitmap(const Bitmap& copy) : Bitmap(copy.width, copy.height, copy.get_row_pointers())
-		{
-		}
-		Bitmap(Bitmap&& move) noexcept :
-			bitmap(std::move(move.bitmap)),
-			row_pointers(std::move(move.row_pointers)),
-			pitch(move.pitch),
-			width(move.width),
-			height(move.height)
-		{
-		}
-		Bitmap& operator =(const Bitmap& to) = default;
-		Bitmap(uint32_t width, uint32_t height) : width(width), height(height), pitch(calc_pitch(width))
-		{
-			build_bitmap();
-		}
-		Bitmap(uint32_t width, uint32_t height, const T& default_color) : width(width), height(height), pitch(calc_pitch(width))
-		{
-			build_bitmap();
-#pragma omp parallel for
-			for (std::ptrdiff_t y = 0; y < static_cast<std::ptrdiff_t>(height); y++)
-			{
-				auto dst_row = row_pointers[y];
-				for (size_t x = 0; x < width; x++)
-				{
-					dst_row[x] = default_color;
-				}
-			}
-		}
-		Bitmap(uint32_t width, uint32_t height, const T*const* from_row_pointers) : width(width), height(height), pitch(calc_pitch(width))
-		{
-			build_bitmap();
-#pragma omp parallel for
-			for (std::ptrdiff_t y = 0; y < static_cast<std::ptrdiff_t>(height); y++)
-			{
-				auto src_row = from_row_pointers[y];
-				auto dst_row = row_pointers[y];
-				for (size_t x = 0; x < width; x++)
-				{
-					dst_row[x] = src_row[x];
-				}
-			}
-		}
-		T* get_bitmap() { return &bitmap[0]; }
-		const T* get_bitmap() const { return &bitmap[0]; }
-		T** get_row_pointers() { return &row_pointers[0]; }
-		const T*const* get_row_pointers() const { return &row_pointers[0]; }
-		T* get_row(size_t y) { return row_pointers[y]; }
-		const T* get_row(size_t y) const { return row_pointers[y]; }
-		T& get_pixel(uint32_t x, uint32_t y) { return row_pointers[y][x]; }
-		const T& get_pixel(uint32_t x, uint32_t y) const { return row_pointers[y][x]; }
-		void set_pixel(uint32_t x, uint32_t y, const T& v) { row_pointers[y][x] = v; }
+		~Bitmap();
+		Bitmap(const Bitmap& copy);
+		Bitmap(Bitmap&& move) noexcept;
+		Bitmap& operator =(const Bitmap& to);
+		Bitmap(uint32_t width, uint32_t height);
+		Bitmap(uint32_t width, uint32_t height, const T& default_color);
+		Bitmap(uint32_t width, uint32_t height, const T*const* from_row_pointers);
 
-		uint32_t get_width() const {return width;}
-		uint32_t get_height() const { return height; }
-		size_t get_pitch() const { return pitch; }
+		T* get_bitmap()
+		{
+			return bitmap;
+		}
+		const T* get_bitmap() const
+		{
+			return bitmap;
+		}
+		T** get_row_pointers()
+		{
+			return &row_pointers[0];
+		}
+		const T*const* get_row_pointers() const
+		{
+			return &row_pointers[0];
+		}
+		T* get_row(size_t y)
+		{
+			return row_pointers[y];
+		}
+		const T* get_row(size_t y) const
+		{
+			return row_pointers[y];
+		}
+		T& get_pixel(uint32_t x, uint32_t y)
+		{
+			return row_pointers[y][x];
+		}
+		const T& get_pixel(uint32_t x, uint32_t y) const
+		{
+			return row_pointers[y][x];
+		}
+		void set_pixel(uint32_t x, uint32_t y, const T& v)
+		{
+			row_pointers[y][x] = v;
+		}
+
+		uint32_t get_width() const
+		{
+			return width;
+		}
+		uint32_t get_height() const
+		{
+			return height;
+		}
+		size_t get_pitch() const
+		{
+			return pitch;
+		}
 
 		template<typename T_dst>
-		Bitmap<T_dst> convert(T_dst(*convert_fn)(T& src)) const
-		{
-			auto ret = Bitmap<T_dst>(width, height);
-			auto ret_row = ret.get_row_pointers();
-#pragma omp parallel for
-			for (std::ptrdiff_t y = 0; y < static_cast<std::ptrdiff_t>(height); y++)
-			{
-				auto src_row = row_pointers[y];
-				auto dst_row = ret_row[y];
-				for (size_t x = 0; x < width; x++)
-				{
-					dst_row[x] = convert_fn(src_row[x]);
-				}
-			}
-
-			return ret;
-		}
+		Bitmap<T_dst> convert(T_dst(*convert_fn)(T& src)) const;
 	};
 
-	extern template class Bitmap<int     >;
-	extern template class Bitmap<uint8_t >;
-	extern template class Bitmap<ColorRgb >;
-	extern template class Bitmap<ColorRgba >;
+	extern template class Bitmap<int>;
+	extern template class Bitmap<uint8_t>;
+	extern template class Bitmap<ColorRgb>;
+	extern template class Bitmap<ColorRgba>;
 	extern template class Bitmap<IColorRgb>;
 	extern template class Bitmap<IColorRgba>;
 
